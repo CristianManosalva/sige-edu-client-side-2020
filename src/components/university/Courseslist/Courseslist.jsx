b452073699fc62c12a0b2e3c00cf7f9df7228ad4
Igor Ariza <igorariza@gmail.com>
2020-05-07 16:56:21 -0500

    refactor:  project

--- src/components/university/Courseslist/Courseslist.jsx
+++ src/components/university/Courseslist/Courseslist.jsx
@@ -54,46 +54,55 @@ const Courseslist = (props) => {
           //     </div>
           //   </div>
 
-<div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
-<div className="team-member">
-  <div className="team-img">
-  <img
-                    className="img-fluid"
-                    src={tempImg[0]}
-                    alt=""
-                    style={{ objectFit: 'cover' }}
-                  />
-  </div>
-  <div className="team-info">
-    <h3>
-    <NavLink
-      to={
-        BASEDIR +
-        `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
-      }
-    >
-      {group.nameGroup
-        ? group.nameGroup.split('-')[0] +
-          '-' +
-          group.nameGroup.split('-')[1]
-        : 'Grupo *?*?'}{' '}
-      {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
-    </NavLink>
-    </h3>
-    <span>Jornada </span>
-    <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
-    <p>{group.msg}</p>
-    <ul className="social-icons list-inline list-unstyled">
-      <li className="list-inline-item">
-        <a href="#!">
-          <i className="i-envelope icon-primary icon-xs"></i>
-        </a>
-      </li>
-    </ul>
-  </div>
-</div>
-</div>
-       )
+          <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
+            <div className="team-member">
+              <div className="team-img">
+                <img
+                  className="img-fluid"
+                  src={tempImg[0]}
+                  alt=""
+                  style={{ objectFit: 'cover' }}
+                />
+              </div>
+              <div className="team-info">
+                <h3>
+                  <NavLink
+                    to={
+                      BASEDIR +
+                      `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
+                    }
+                  >
+                    {
+                      group.nameGroup
+                      ? group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1]
+                      : 'Grupo *?*?'
+                    }{' '}
+
+                    {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
+                    
+                    
+                    {/* {group.nameGroup
+                      ? group.nameGroup.split('-')[0] +
+                      '-' +
+                      group.nameGroup.split('-')[1]
+                      : 'Grupo *?*?'}{' '}
+                    {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'} */}
+                  </NavLink>
+                </h3>
+                <span>Jornada </span>
+                <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
+                <p>{group.msg}</p>
+                <ul className="social-icons list-inline list-unstyled">
+                  <li className="list-inline-item">
+                    <a href="#!">
+                      <i className="i-envelope icon-primary icon-xs"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        )
       })}
     </div>
   )
