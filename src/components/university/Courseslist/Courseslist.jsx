ba55e81b5a491c6158be3ed0491f4f595f6ae023
Igor Ariza <igorariza@gmail.com>
2020-05-08 11:54:12 -0500

    fix: format name group

--- src/components/university/Courseslist/Courseslist.jsx
+++ src/components/university/Courseslist/Courseslist.jsx
@@ -6,32 +6,40 @@ var BASEDIR = process.env.REACT_APP_BASEDIR
 
 
 const Courseslist = (props) => {
-  const { teacher_id, materia_id } = props.user
+  const { teacher_id, materia_id} = props.user
   const tempImg =
-    ['https://images.pexels.com/photos/2170/creative-desk-pens-school.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 'https://images.pexels.com/photos/1053687/pexels-photo-1053687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
+    ['https://images.pexels.com/photos/2170/creative-desk-pens-school.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+    'https://images.pexels.com/photos/1053687/pexels-photo-1053687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/632470/pexels-photo-632470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/256520/pexels-photo-256520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/159497/school-notebook-binders-notepad-159497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/167682/pexels-photo-167682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/298660/pexels-photo-298660.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
   return (
     <div className="row">
       {props.courses.map((group, key) => {
-        
-        var jornada = group.
-        
+        var nameJourneyGroup
+        if(group.journeyGroup == 1){
+          nameJourneyGroup = 'Mañana'
+        }else if(group.journeyGroup == 2) {
+          nameJourneyGroup = 'Tarde'
+        }else{
+          nameJourneyGroup = '-----'
+        }
+        // var nameGroup
+        // if(group.nameGroup)
 
-        
-            
-        var nameGroupCourse = group.nameGroup 
-                              ? group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1]
-                              : '----'
-        
         return (
-          
-          
-          
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
             <div className="team-member">
               <div className="team-img">
                 <img
                   className="img-fluid"
-                  src={tempImg[0]}
+                  src={tempImg[key]}
                   alt=""
                   style={{ objectFit: 'cover' }}
                 />
@@ -44,11 +52,11 @@ const Courseslist = (props) => {
                       `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
                     }
                   >
-                    {nameGroupCourse + jornada}{' '}
+                    {group.nameGroup.spl}
                   </NavLink>
                 </h3>
                 <span>Jornada </span>
-                <span>{}</span>
+                <span>{nameJourneyGroup}</span>
                 <p>{group.msg}</p>
                 <ul className="social-icons list-inline list-unstyled">
                   <li className="list-inline-item">
