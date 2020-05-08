1e586221d2ece631d6e6e7007cac1e77403b6d5b
Igor Ariza <igorariza@gmail.com>
2020-05-08 10:49:54 -0500

    fix: format name group

--- src/components/university/Courseslist/Courseslist.jsx
+++ src/components/university/Courseslist/Courseslist.jsx
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 
 var BASEDIR = process.env.REACT_APP_BASEDIR
 
+
 const Courseslist = (props) => {
   const { teacher_id, materia_id } = props.user
   const tempImg =
@@ -11,49 +12,26 @@ const Courseslist = (props) => {
   return (
     <div className="row">
       {props.courses.map((group, key) => {
+        
+        var jornada = (() => (
+          var nameJourney = '----'
+          if(group.journeyGroup === 1){
+            nameJourney = 'Mañana'
+          }else if(group.journeyGroup === 2){
+            nameJourney = 'Tarde'
+          }
+          return nameJourney
+        ))}
+        
+            
+        var nameGroupCourse = group.nameGroup 
+                              ? group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1]
+                              : '----'
+        
         return (
-
-          // <div className="col-md-6 col-lg-6 col-sm-12" key={key}>
-          //   <div className="team-member">
-          //     <div className="team-img">
-          //         {/* <img className="img-fluid" src={group.avatar} alt="" /> */}
-          //         <img
-          //           className="img-fluid"
-          //           src={tempImg}
-          //           alt=""
-          //           style={{ objectFit: 'cover' }}
-          //         />
-          //     </div>
-          //       <div className="team-info">
-          //         <h3>
-          //           <NavLink
-          //             to={
-          //               BASEDIR +
-          //               `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
-          //             }
-          //           >
-          //             {group.nameGroup
-          //               ? group.nameGroup.split('-')[0] +
-          //                 '-' +
-          //                 group.nameGroup.split('-')[1]
-          //               : 'Grupo *?*?'}{' '}
-          //             {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
-          //           </NavLink>
-          //         </h3>
-          //         <span>Jornada </span>
-          //         <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
-          //         {/* <ul className="social-icons list-inline list-unstyled"> Se podria usar para lincar estadisticas
-          //           <li className="list-inline-item">
-          //             <a href="#!">
-          //               <i className="i-envelope icon-primary icon-xs"></i>
-          //             </a>
-          //           </li>
-          //         </ul> */}
-          //         <p>{group.msg}</p>
-          //       </div>
-          //     </div>
-          //   </div>
-
+          
+          
+          
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
             <div className="team-member">
               <div className="team-img">
@@ -72,25 +50,11 @@ const Courseslist = (props) => {
                       `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
                     }
                   >
-                    {
-                      group.nameGroup
-                      ? group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1]
-                      : 'Grupo *?*?'
-                    }{' '}
-
-                    {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
-                    
-                    
-                    {/* {group.nameGroup
-                      ? group.nameGroup.split('-')[0] +
-                      '-' +
-                      group.nameGroup.split('-')[1]
-                      : 'Grupo *?*?'}{' '}
-                    {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'} */}
+                    {nameGroupCourse + jornada}{' '}
                   </NavLink>
                 </h3>
                 <span>Jornada </span>
-                <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
+                <span>{jornada}</span>
                 <p>{group.msg}</p>
                 <ul className="social-icons list-inline list-unstyled">
                   <li className="list-inline-item">
