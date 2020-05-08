946fa1556f23fdbd3da523c09847d0f171f674fc
Igor Ariza <igorariza@gmail.com>
2020-05-08 12:29:48 -0500

    fix: format name group

--- src/components/university/Courseslist/Courseslist.jsx
+++ src/components/university/Courseslist/Courseslist.jsx
@@ -18,7 +18,19 @@ const Courseslist = (props) => {
     'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     'https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
     'https://images.pexels.com/photos/298660/pexels-photo-298660.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
+    'https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/2170/creative-desk-pens-school.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940', 
+    'https://images.pexels.com/photos/1053687/pexels-photo-1053687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/632470/pexels-photo-632470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/256520/pexels-photo-256520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/159497/school-notebook-binders-notepad-159497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/167682/pexels-photo-167682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/298660/pexels-photo-298660.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    'https://images.pexels.com/photos/632470/pexels-photo-632470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940']
   return (
     <div className="row">
       {props.courses.map((group, key) => {
@@ -30,9 +42,14 @@ const Courseslist = (props) => {
         }else{
           nameJourneyGroup = '-----'
         }
-        // var nameGroup
-        // if(group.nameGroup)
-
+        var nameGroup = group.nameGroup 
+                        // ? group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1] 
+                        // : '-----'
+        if(nameGroup.substr(0,1) == 'T'){
+          nameGroup = group.nameGroup.split('-')[1] + '-' + group.nameGroup.split('-')[2] 
+        }else{
+          nameGroup = group.nameGroup.split('-')[0] + '-' + group.nameGroup.split('-')[1]
+        }
         return (
           <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
             <div className="team-member">
@@ -52,7 +69,7 @@ const Courseslist = (props) => {
                       `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
                     }
                   >
-                    {group.nameGroup.split('-')[1]}
+                    {nameGroup}
                   </NavLink>
                 </h3>
                 <span>Jornada </span>
