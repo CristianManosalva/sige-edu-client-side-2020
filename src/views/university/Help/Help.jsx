68c1567ebc76bb856413a68df25ea715005716eb
dianag19 <diana.gomez.caicedo@correounivalle.edu.co>
2020-05-10 07:03:53 -0500

    ifram de youtube en help

--- src/views/university/Help/Help.jsx
+++ src/views/university/Help/Help.jsx
@@ -22,9 +22,13 @@ const Help = () => {
                 */}
                 <div className="content-body">
                   <div className="row">
-                    <div className="col-12">
-                      <h3>Próximamente tendrémos disponible material de ayuda...</h3>
-                    </div>
+                    <iframe
+                      src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+                      frameBorder="0"
+                      allow="autoplay; encrypted-media"
+                      allowFullScreen
+                      title="video"
+                    />
                   </div>
                 </div>
               </section>
@@ -34,9 +38,9 @@ const Help = () => {
       </div>
     </div>
 
-// <div className="row">
-//{props.courses.map((group, key) => {
-  //return ( */}
+    // <div className="row">
+    //{props.courses.map((group, key) => {
+    //return ( */}
 
     // <div className="col-md-6 col-lg-6 col-sm-12" key={key}>
     //   <div className="team-member">
@@ -79,48 +83,48 @@ const Help = () => {
     //     </div>
     //   </div>
 
-// <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
-// <div className="team-member">
-// <div className="team-img">
-// <img
-//               className="img-fluid"
-//               src={tempImg}
-//               alt=""
-//               style={{ objectFit: 'cover' }}
-//             />
-// </div>
-// <div className="team-info">
-// <h3>
-// <NavLink
-// to={
-//   BASEDIR +
-//   `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
-// }
-// >
-// {group.nameGroup
-//   ? group.nameGroup.split('-')[0] +
-//     '-' +
-//     group.nameGroup.split('-')[1]
-//   : 'Grupo *?*?'}{' '}
-// {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
-// </NavLink>
-// </h3>
-// <span>Jornada </span>
-// <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
-// <p>{group.msg}</p>
-// <ul className="social-icons list-inline list-unstyled">
-// <li className="list-inline-item">
-//   <a href="#!">
-//     <i className="i-envelope icon-primary icon-xs"></i>
-//   </a>
-// </li>
-// </ul>
-// </div>
-// </div>
-// </div>
-//  )
-// })}
-// </div>
+    // <div className="col-xs-12 col-sm-6 col-md-4 col-lg-4 col-xl-3" key={key}>
+    // <div className="team-member">
+    // <div className="team-img">
+    // <img
+    //               className="img-fluid"
+    //               src={tempImg}
+    //               alt=""
+    //               style={{ objectFit: 'cover' }}
+    //             />
+    // </div>
+    // <div className="team-info">
+    // <h3>
+    // <NavLink
+    // to={
+    //   BASEDIR +
+    //   `/university/activity/${teacher_id}/${group.nameGroup}/${materia_id}`
+    // }
+    // >
+    // {group.nameGroup
+    //   ? group.nameGroup.split('-')[0] +
+    //     '-' +
+    //     group.nameGroup.split('-')[1]
+    //   : 'Grupo *?*?'}{' '}
+    // {group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}
+    // </NavLink>
+    // </h3>
+    // <span>Jornada </span>
+    // <span>{group.journeyGroup === 1 ? 'Mañana' : 'Tarde'}</span>
+    // <p>{group.msg}</p>
+    // <ul className="social-icons list-inline list-unstyled">
+    // <li className="list-inline-item">
+    //   <a href="#!">
+    //     <i className="i-envelope icon-primary icon-xs"></i>
+    //   </a>
+    // </li>
+    // </ul>
+    // </div>
+    // </div>
+    // </div>
+    //  )
+    // })}
+    // </div>
   )
 }
 
