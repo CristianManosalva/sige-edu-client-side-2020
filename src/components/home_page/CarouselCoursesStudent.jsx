a579b7b2ca74fe1ac8edc6c6293f121df0d417fe
Igor Ariza <igorariza@gmail.com>
2020-06-05 10:12:12 -0500

    feat: courses student carousel

---
+++ src/components/home_page/CarouselCoursesStudent.jsx
@@ -0,0 +1,52 @@
+import React, { Fragment, useState, useEffect } from 'react'
+// import { Link, Image } from './stylesCarouselCoursesStudent'
+// const teacherId = '109'
+// const groupID = '6-01-01-2020-1'
+// const [activityStudent, setActivityStudent] = useState(null);
+// useEffect(() => {
+//   fetch(`http://api.sige-edu.com:8000/api/workspaces/courses/${teacherId}/${groupID}`)
+//     .then(res => res.json())
+//     .then(response => {
+//       setActivityStudent(response)
+//     })
+// }, [])
+
+
+const CarouselCoursesStudent = (props) => {
+  let urlImage = props.urlImage
+  let title = props.title
+  let idTeacher = props.idTeacher
+  let idGroup = props.idGroup
+  // console.log(props);
+  const ActivityCourses = (title) => {
+
+
+    return (
+      <div className="news-item" >
+        {console.log('click', title)}
+        {/* <img className="news-item__imagen" src={urlImage} alt="" />
+        <a href="#ss">{title}</a> */}
+      </div>
+    )
+  }
+  
+
+  const carouselCourses = () => {
+
+    return (
+      <div className="news-item" >
+        <img className="news-item__imagen" src={urlImage} alt="" />
+        <a>{title} </a>
+      </div>
+    )
+  }
+
+ 
+
+  return (
+    <Fragment>
+      {carouselCourses()}
+    </Fragment>
+  )
+}
+export default CarouselCoursesStudent;
--- src/views/hospital/Doctor/CoursesStudent.jsx
+++ src/views/hospital/Doctor/CoursesStudent.jsx
@@ -1,35 +1,12 @@
 import React, { Fragment, useState, useEffect } from 'react'
 import { Row, Col, } from 'reactstrap';
-import CoursesStudentList from '../../../components/hospital/Doctorslist/CoursesStudentList';
-import { doctors } from 'variables/hospital/doctors.jsx';
+import { Router } from '@reach/router'
 import useCoursesStudent from '../../../hooks/useCoursesStudent'
 import { useSelector } from 'react-redux'
-import { Carousel, NewsCarouselItem, ClientCarouselItem } from 'components'
-
-let codeStudent = ''
-try {
-    codeStudent = useSelector((state) => state.authentication.user.user_data.student)
-}
-catch (error) {
-    codeStudent = ''
-}
-
-const tempImg = [
-    'https://images.pexels.com/photos/2170/creative-desk-pens-school.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/1053687/pexels-photo-1053687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/632470/pexels-photo-632470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/247819/pexels-photo-247819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/256520/pexels-photo-256520.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/159497/school-notebook-binders-notepad-159497.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/167682/pexels-photo-167682.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/265076/pexels-photo-265076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/298660/pexels-photo-298660.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/256517/pexels-photo-256517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/2170/creative-desk-pens-school.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/1053687/pexels-photo-1053687.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    'https://images.pexels.com/photos/632470/pexels-photo-632470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-]
+import CarouselCoursesStudent from '../../../components/home_page/CarouselCoursesStudent'
+import { Carousel } from 'components'
+import ListOfActivityCards from '../../../components/hospital/Doctorslist/ListOfActivityCards'
+var IMGDIR = process.env.REACT_APP_IMGDIR;
 const CoursesStudent = (props) => {
     const { student } = useSelector((state) => state.authentication.user.user_data)
     const student_id = student.codeStudent
@@ -39,20 +16,7 @@ const CoursesStudent = (props) => {
     const renderCoursesStudentList = (fixed) => (
         <div>
             <div className="content">
-            <h1 className="title">Asignaturas</h1>
-
-                <Carousel>
-                    {[1, 2, 3, 4,5,6,7].map((id, key) => {
-                        return (
-                            <NewsCarouselItem
-                                key={key}
-                                urlImage={tempImg[id]}
-                                title={'Matematicas'}
-                            />
-                        )
-                    })}
-                </Carousel>
-
+                <h1 className="title">Asignaturas</h1>
                 <Row>
                     <Col xs={12} md={12}>
                         <div className="col-xl-12">
@@ -60,7 +24,25 @@ const CoursesStudent = (props) => {
                                 <div className="content-body">
                                     <div className="row">
                                         <div className="col-12">
-                                            <CoursesStudentList courses={courses} loading={loading} />
+                                            < div className="row">
+                                                {console.log(courses)}
+                                                <Carousel>
+                                                    {courses.map((courses, key) => {
+                                                        let idAcademicCharge = courses.codeAcademicCharge
+                                                        let courseDictate = courses.groupDictate
+
+                                                        return (
+                                                            <CarouselCoursesStudent
+                                                                key={key}
+                                                                urlImage={IMGDIR + `/images/cards/${key}.jpeg`}
+                                                                idTeacher={courses.teacherDictate}
+                                                                idGroup={'6-01-01-2020-1'}
+                                                                title={courses.courseDictate.nameCourse}
+                                                            />
+                                                        )
+                                                    })}
+                                                </Carousel>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
