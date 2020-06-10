3460059d8a16555d2378e3b7db1883e88e0ff6b6
Igor Ariza <igorariza@gmail.com>
2020-06-09 23:18:03 -0500

    feat: view activity students U|X

--- src/components/hospital/Doctorslist/ListOfActivityCards.jsx
+++ src/components/hospital/Doctorslist/ListOfActivityCards.jsx
@@ -1,26 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import SkeletonTeacherHome from '../../skeleton/SkeletonTeacherHome'
-import { 
-    Divcardactivity, 
-    Coursescontainer, 
-    Coursedetail, 
-    Coursepreview, 
-    Courseinfo, 
-    Coursestudent, 
-    Progrescontainer, 
-    Buttonrevisar, 
-    Progress, 
-    Progresstext, Textcourse } from './stylesActivityCourse'
+import {
+    Divcardactivity,
+    Coursescontainer,
+    Coursedetail,
+    Coursepreview,
+    Courseinfo,
+    Coursestudent,
+    Progrescontainer,
+    Buttonrevisar,
+    Progress,
+    Progresstext,
+    Textcourse,
+    Description,
+    Resource,
+    Buttonrecursos
+} from './stylesActivityCourse'
 import { Button, Collapse, Card, CardBody } from 'reactstrap'
+import Scrollbar from "react-scrollbars-custom";
 // import CarouselStundet
 var IMGDIR = process.env.REACT_APP_IMGDIR;
-
 const ListOfActivityCards = (props) => {
-    
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
     var nameSection = props.value.nameSecction
     var descriptionSection = props.value.descriptionSecction
-    
-    console.log('valueEventAriaMessage..',props);
+
+    console.log('valueEventAriaMessage..', props);
     let courses = props.courses
     if (props.loading) {
         return <SkeletonTeacherHome />
@@ -30,12 +37,33 @@ const ListOfActivityCards = (props) => {
         return (
             <Divcardactivity>
                 <Coursedetail>
-                <Coursepreview>
+
+                    <Coursepreview>
                         <Coursestudent>Lengua Castellana</Coursestudent>
                         <Textcourse>{nameSection}</Textcourse>
+                        <Scrollbar>
+                            <Resource style={{ paddingTop: '10px' }}>
+                            <Buttonrecursos color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Recursos</Buttonrecursos>
+                                
+                                <Collapse isOpen={isOpen}>
+                                <Buttonrecursos color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Archivos</Buttonrecursos>
+                                <Buttonrecursos color="primary" onClick={toggle} style={{ marginBottom: '1rem' }}>Link's</Buttonrecursos>
+                                    {/* <Card>
+                                        <CardBody>
+                                            Anim pariatur cliche reprehenderit,
+                                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                                            nesciunt sapiente ea proident.
+                                        </CardBody>
+                                    </Card> */}
+                                </Collapse>
+
+                            </Resource>
+                        </Scrollbar>
                         <a href="#">Documentos</a>
 
                     </Coursepreview>
+
                     <Courseinfo>
                         <Progrescontainer>
                             <Progress></Progress>
@@ -44,11 +72,13 @@ const ListOfActivityCards = (props) => {
 				            </Progresstext>
                         </Progrescontainer>
                         {/* <h6>Chapter 4</h6> */}
-                        <h2>{descriptionSection}</h2>
+                        <Scrollbar>
+                            <Description>{descriptionSection}</Description>
+                        </Scrollbar>
                         <Buttonrevisar>Enviar respuesta</Buttonrevisar>
                     </Courseinfo>
                 </Coursedetail>
-            </Divcardactivity>
+            </Divcardactivity >
         )
     }
 
